1ea6ebc828ab44f6167fa4a34c471bb062a7a417
lilai <lilai23@foxmail.com>
2021-11-05 09:45:16 +0800

    feign灰度插件迁移

---
+++ javamesh-samples/javamesh-route/gray-feign-http-9.x-plugin/src/main/java/com/huawei/gray/feign/definition/HttpClientDefinition.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
+ */
+
+package com.huawei.gray.feign.definition;
+
+import com.huawei.apm.core.agent.definition.EnhanceDefinition;
+import com.huawei.apm.core.agent.definition.MethodInterceptPoint;
+import com.huawei.apm.core.agent.matcher.ClassMatcher;
+import com.huawei.apm.core.agent.matcher.ClassMatchers;
+
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.matcher.ElementMatchers;
+
+/**
+ * 拦截feign的ApacheHttpClient和OkHttpClient请求
+ *
+ * @author lilai
+ * @since 2021-11-03
+ */
+public class HttpClientDefinition implements EnhanceDefinition {
+    private static final String ENHANCE_CLASS_APACHE_HTTP_CLIENT = "feign.httpclient.ApacheHttpClient";
+
+    private static final String ENHANCE_CLASS_OK_HTTP_CLIENT = "feign.okhttp.OkHttpClient";
+
+    /**
+     * Intercept class.
+     */
+    private static final String INTERCEPT_CLASS = "com.huawei.gray.feign.interceptor.DefaultHttpClientInterceptor";
+
+    @Override
+    public ClassMatcher enhanceClass() {
+        return ClassMatchers.multiClass(ENHANCE_CLASS_APACHE_HTTP_CLIENT, ENHANCE_CLASS_OK_HTTP_CLIENT);
+    }
+
+    @Override
+    public MethodInterceptPoint[] getMethodInterceptPoints() {
+        return new MethodInterceptPoint[]{
+                MethodInterceptPoint.newInstMethodInterceptPoint(
+                        INTERCEPT_CLASS, ElementMatchers.<MethodDescription>named("execute")
+                )
+        };
+    }
+}
+
