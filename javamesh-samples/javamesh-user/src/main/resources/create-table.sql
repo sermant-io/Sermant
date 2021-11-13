31b9818827568a84085244c1fcd2ffcaf227d56f
h30009881 <hujialiang4@huawei.com>
2021-11-13 14:22:27 +0800

    应急演练

--- javamesh-samples/javamesh-user/src/main/resources/create-table.sql
+++ javamesh-samples/javamesh-user/src/main/resources/create-table.sql
@@ -1,7 +1,47 @@
 CREATE TABLE IF NOT EXISTS `emergency_auth`
 (
-    `auth_id`   int(11) NOT NULL AUTO_INCREMENT COMMENT '权限ID',
-    `role_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
-    `auth_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限名',
-    PRIMARY KEY (`auth_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+    `auth_id` int
+(
+    11
+) NOT NULL AUTO_INCREMENT COMMENT '权限ID',
+    `role_name` varchar
+(
+    255
+) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色名',
+    `auth_name` varchar
+(
+    255
+) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限名',
+    PRIMARY KEY
+(
+    `auth_id`
+) USING BTREE
+    ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of emergency_auth
+-- ----------------------------
+insert into `emergency_auth` (role_name, auth_name)
+select 'ADMIN', 'admin'
+FROM DUAL
+WHERE NOT EXISTS(SELECT role_name, auth_name FROM `emergency_auth` WHERE role_name = 'ADMIN' AND auth_name = 'admin');
+insert into `emergency_auth` (role_name, auth_name)
+select 'ADMIN', 'operator'
+FROM DUAL
+WHERE NOT EXISTS(SELECT role_name, auth_name FROM `emergency_auth` WHERE role_name = 'ADMIN' AND auth_name = 'operator');
+
+insert into `emergency_auth` (role_name, auth_name)
+select 'ADMIN', 'approver'
+FROM DUAL
+WHERE NOT EXISTS(SELECT role_name, auth_name FROM `emergency_auth` WHERE role_name = 'ADMIN' AND auth_name = 'approver');
+
+insert into `emergency_auth` (role_name, auth_name)
+select 'APPROVER', 'approver'
+FROM DUAL
+WHERE NOT EXISTS(SELECT role_name, auth_name FROM `emergency_auth` WHERE role_name = 'APPROVER' AND auth_name = 'approver');
+
+insert into `emergency_auth` (role_name, auth_name)
+select 'OPERATOR', 'operator'
+FROM DUAL
+WHERE NOT EXISTS(SELECT role_name, auth_name FROM `emergency_auth` WHERE role_name = 'OPERATOR' AND auth_name = 'operator');
+
