48fa104edeb47caf9f53215a5abbbff2875886e2
justforstudy <2167005981@qq.com>
2021-12-03 10:40:01 +0800

    add alibaba sentinel license

--- javamesh-plugins/javamesh-flowcontrol/flowcontrol-server/src/main/java/com/huawei/flowcontrol/console/auth/AuthUser.java
+++ javamesh-plugins/javamesh-flowcontrol/flowcontrol-server/src/main/java/com/huawei/flowcontrol/console/auth/AuthUser.java
@@ -1,19 +1,23 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.huawei.flowcontrol.console.auth;
 
 /**
- * 文件名：AuthUser
- * 版权：
- * 描述：用户bean接口类
- *
- * @author Gaofang Wu
- * @since 2020-11-25
- * 跟踪单号：
- * 修改单号：
- * 修改内容：
+ * 此处部分引用alibaba/Sentinel开源社区代码，诚挚感谢alibaba/Sentinel开源团队的慷慨贡献
  */
 public interface AuthUser {
     /**
