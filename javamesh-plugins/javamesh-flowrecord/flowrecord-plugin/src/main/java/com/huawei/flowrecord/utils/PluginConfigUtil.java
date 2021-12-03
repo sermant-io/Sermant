db97466f186f825b7340f01ae29c68a40fb67ac2
HapThorin <hapthorin@163.com>
2021-12-03 16:34:39 +0800

    Revert "删除skywalking依赖，添加asf copyright"

--- javamesh-plugins/javamesh-flowrecord/flowrecord-plugin/src/main/java/com/huawei/flowrecord/utils/PluginConfigUtil.java
+++ javamesh-plugins/javamesh-flowrecord/flowrecord-plugin/src/main/java/com/huawei/flowrecord/utils/PluginConfigUtil.java
@@ -1,33 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
  */
 
 package com.huawei.flowrecord.utils;
 
-import com.huawei.javamesh.core.common.LoggerFactory;
 import com.huawei.javamesh.core.plugin.config.PluginConfigManager;
 import com.huawei.flowrecord.config.FlowRecordConfig;
-
-import java.util.logging.Logger;
+import org.apache.skywalking.apm.agent.core.logging.api.ILog;
+import org.apache.skywalking.apm.agent.core.logging.api.LogManager;
 
 /**
  * 配置信息工具类
  */
 public class PluginConfigUtil {
-    private static final Logger LOGGER = LoggerFactory.getLogger();
+    private static final ILog LOGGER = LogManager.getLogger(PluginConfigUtil.class);
     private static FlowRecordConfig flowRecordConfig;
 
     static {
