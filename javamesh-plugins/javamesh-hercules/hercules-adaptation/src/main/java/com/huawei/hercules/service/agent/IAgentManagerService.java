2b306ebddecb0df4b5f2c36a91d1fb09183882be
z30009938 <zhonglei26@huawei.com>
2021-12-03 16:01:16 +0800

    前端license格式修改

--- javamesh-plugins/javamesh-hercules/hercules-adaptation/src/main/java/com/huawei/hercules/service/agent/IAgentManagerService.java
+++ javamesh-plugins/javamesh-hercules/hercules-adaptation/src/main/java/com/huawei/hercules/service/agent/IAgentManagerService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Huawei Technologies Co., Ltd. $YEAR$-$YEAR$. All rights reserved
+ * Copyright (C) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
