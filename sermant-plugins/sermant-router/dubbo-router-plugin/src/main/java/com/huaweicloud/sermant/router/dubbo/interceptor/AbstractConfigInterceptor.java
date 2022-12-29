03b154cec37a65b581a8848a65ccd6ecd63bbc8b
chengyouling <spadgerlin888@163.com>
2022-12-29 16:33:34 +0800

    兼容使用dubbo2.5-2.5.8以下版本dubbo标签设置

--- sermant-plugins/sermant-router/dubbo-router-plugin/src/main/java/com/huaweicloud/sermant/router/dubbo/interceptor/AbstractConfigInterceptor.java
+++ sermant-plugins/sermant-router/dubbo-router-plugin/src/main/java/com/huaweicloud/sermant/router/dubbo/interceptor/AbstractConfigInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2022-2022 Huawei Technologies Co., Ltd. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,47 +19,39 @@ package com.huaweicloud.sermant.router.dubbo.interceptor;
 import com.huaweicloud.sermant.core.plugin.agent.entity.ExecuteContext;
 import com.huaweicloud.sermant.core.plugin.agent.interceptor.AbstractInterceptor;
 import com.huaweicloud.sermant.core.plugin.config.PluginConfigManager;
-import com.huaweicloud.sermant.core.utils.StringUtils;
 import com.huaweicloud.sermant.router.common.config.RouterConfig;
-import com.huaweicloud.sermant.router.dubbo.cache.DubboCache;
-import com.huaweicloud.sermant.router.dubbo.utils.DubboReflectUtils;
 import com.huaweicloud.sermant.router.dubbo.utils.ParametersUtils;
 
-import java.util.HashMap;
+import com.alibaba.dubbo.config.ApplicationConfig;
+
 import java.util.Map;
 
 /**
- * 增强ApplicationConfig类的setName方法，用来获取应用名
+ * 增强AbstractConfig类的appendParameters方法，增加路由标签
  *
- * @author provenceee
- * @since 2021-11-08
+ * @author chengyouling
+ * @since 2022-12-28
  */
-public class ApplicationConfigInterceptor extends AbstractInterceptor {
+public class AbstractConfigInterceptor extends AbstractInterceptor {
     private final RouterConfig routerConfig;
 
     /**
      * 构造方法
      */
-    public ApplicationConfigInterceptor() {
+    public AbstractConfigInterceptor() {
         routerConfig = PluginConfigManager.getPluginConfig(RouterConfig.class);
     }
 
     @Override
     public ExecuteContext before(ExecuteContext context) {
-        String name = context.getMethod().getName();
         if (context.getArguments() == null || context.getArguments().length == 0) {
             return context;
         }
-        Object argument = context.getArguments()[0];
-        if ("setName".equals(name)) {
-            if (argument == null || argument instanceof String) {
-                setAppNameAndPutParameters(context.getObject(), (String) argument);
-            }
-        } else {
-            if (argument == null || argument instanceof Map<?, ?>) {
-                context.getArguments()[0] = ParametersUtils.putParameters((Map<String, String>) argument,
-                        routerConfig);
-            }
+        Object parameters = context.getArguments()[0];
+        Object config = context.getArguments()[1];
+        if (parameters instanceof Map<?, ?> && config instanceof ApplicationConfig) {
+            context.getArguments()[0] = ParametersUtils.putParameters((Map<String, String>) parameters,
+                routerConfig);
         }
         return context;
     }
@@ -67,13 +59,5 @@ public class ApplicationConfigInterceptor extends AbstractInterceptor {
     @Override
     public ExecuteContext after(ExecuteContext context) {
         return context;
-    }
-
-    private void setAppNameAndPutParameters(Object config, String name) {
-        if (StringUtils.isBlank(name)) {
-            return;
-        }
-        DubboCache.INSTANCE.setAppName(name);
-        DubboReflectUtils.setParameters(config, new HashMap<>());
     }
 }
