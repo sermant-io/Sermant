2e4de3fbda818059571bc808faf7f1d6e5cf790c
daizhenyu <1449308021@qq.com>
2023-09-16 16:37:53 +0800

    流量标签透传插件：优化servicecomb服务端拦截场景

---
+++ sermant-plugins/sermant-tag-transmission/tag-transmission-service/src/main/java/com/huaweicloud/sermant/tag/transmission/service/ServiceCombHeaderParseServiceImpl.java
@@ -0,0 +1,34 @@
+/*
+ *   Copyright (C) 2023-2023 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.huaweicloud.sermant.tag.transmission.service;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.Map;
+
+/**
+ * HeaderParseService的实现类，用于解析servicecomb rpc服务的header
+ *
+ * @author daizhenyu
+ * @since 2023-09-14
+ **/
+public class ServiceCombHeaderParseServiceImpl implements ServiceCombHeaderParseService {
+    @Override
+    public Map<String, String> parseHeaderFromJson(String header) {
+        return JSON.parseObject(header, Map.class);
+    }
+}
