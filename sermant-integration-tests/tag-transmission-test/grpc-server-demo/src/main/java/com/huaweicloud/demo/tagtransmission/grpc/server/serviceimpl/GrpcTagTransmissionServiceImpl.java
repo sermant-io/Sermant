d67b6f984c7df90547ecb99943e3a47094a849d3
daizhenyu <1449308021@qq.com>
2023-10-25 14:16:33 +0800

    流量标签透传插件集成测试demo：grpc demo、crossthread demo、util demo

---
+++ sermant-integration-tests/tag-transmission-test/grpc-server-demo/src/main/java/com/huaweicloud/demo/tagtransmission/grpc/server/serviceimpl/GrpcTagTransmissionServiceImpl.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (C) 2023-2023 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.huaweicloud.demo.tagtransmission.grpc.server.serviceimpl;
+
+import com.huaweicloud.demo.tagtransmission.grpc.api.service.EmptyRequest;
+import com.huaweicloud.demo.tagtransmission.grpc.api.service.TagTransmissionServiceGrpc;
+import com.huaweicloud.demo.tagtransmission.grpc.api.service.TrafficTag;
+import com.huaweicloud.demo.tagtransmission.util.HttpClientUtils;
+
+import io.grpc.stub.StreamObserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * grpc服务的实现类
+ *
+ * @author daizhenyu
+ * @since 2023-10-08
+ **/
+@Component
+public class GrpcTagTransmissionServiceImpl extends TagTransmissionServiceGrpc.TagTransmissionServiceImplBase {
+    @Value("${common.server.url}")
+    private String commonServerUrl;
+
+    @Override
+    public void transmitTag(EmptyRequest request, StreamObserver<TrafficTag> responseObserver) {
+        responseObserver.onNext(TrafficTag.newBuilder()
+                .setTag(HttpClientUtils.doHttpUrlConnectionGet(commonServerUrl))
+                .build());
+        responseObserver.onCompleted();
+    }
+}
