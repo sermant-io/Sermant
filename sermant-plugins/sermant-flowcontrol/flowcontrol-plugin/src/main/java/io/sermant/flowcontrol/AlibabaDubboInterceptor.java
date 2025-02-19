fdd0378bcd70decf87246ecaa62068a7b40a40b8
chengyouling <spadgerlin888@163.com>
2025-02-19 16:19:22 +0800

    support dubbo3.x rateLimiting/circuitBreaker
    Signed-off-by: chengyouling <spadgerlin888@163.com>
    (cherry picked from commit dfa3bd203cdf90f05ae798db3e2b4ae2eb40d247)

--- sermant-plugins/sermant-flowcontrol/flowcontrol-plugin/src/main/java/io/sermant/flowcontrol/AlibabaDubboInterceptor.java
+++ sermant-plugins/sermant-flowcontrol/flowcontrol-plugin/src/main/java/io/sermant/flowcontrol/AlibabaDubboInterceptor.java
@@ -1,18 +1,17 @@
 /*
- * Copyright (C) 2022-2022 Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (C) 2022-2025 Sermant Authors. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package io.sermant.flowcontrol;
@@ -83,7 +82,8 @@ public class AlibabaDubboInterceptor extends InterceptorSupporter {
         // versions and interfaces are used here to achieve the same end result
         String apiPath = ConvertUtils.buildApiPath(interfaceName, version, methodName);
         final boolean isProvider = isProvider(curInvoker);
-        return new DubboRequestEntity(apiPath, DubboAttachmentsHelper.resolveAttachments(invocation, false),
+        return new DubboRequestEntity(apiPath,
+                DubboAttachmentsHelper.resolveAttachments(invocation, false, isProvider),
                 isProvider ? RequestType.SERVER : RequestType.CLIENT, getApplication(url, interfaceName, isProvider),
                 isGeneric);
     }
